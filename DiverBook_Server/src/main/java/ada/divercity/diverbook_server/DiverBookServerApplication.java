72f2ef580c6710b852ce5445c743ce33a837dd76
Yang Sijun <yang@sijun.dev>
2025-04-20 00:59:43 +0900

    Initialize Spring Boot Project

---
+++ DiverBook_Server/src/main/java/ada/divercity/diverbook_server/DiverBookServerApplication.java
@@ -0,0 +1,13 @@
+package ada.divercity.diverbook_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiverBookServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiverBookServerApplication.class, args);
+    }
+
+}
---
+++ DiverBook_Server/src/test/java/ada/divercity/diverbook_server/DiverBookServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ada.divercity.diverbook_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiverBookServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
