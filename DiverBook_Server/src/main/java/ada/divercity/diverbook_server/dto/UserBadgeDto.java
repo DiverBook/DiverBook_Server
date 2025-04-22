b33c4ed2f25c7ba51d67c4e231f6ef8126f35065
Yang Sijun <yang@sijun.dev>
2025-04-22 19:48:51 +0900

    feat: User, Collection, Badge, UserBadge Dto 클래스 구현

---
+++ DiverBook_Server/src/main/java/ada/divercity/diverbook_server/dto/UserBadgeDto.java
@@ -0,0 +1,29 @@
+package ada.divercity.diverbook_server.dto;
+
+import ada.divercity.diverbook_server.entity.Badge;
+import ada.divercity.diverbook_server.entity.User;
+import ada.divercity.diverbook_server.entity.UserBadge;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserBadgeDto {
+    private Long id;
+    private User user;
+    private Badge badge;
+    private LocalDateTime acquiredDate;
+
+    public UserBadgeDto fromEntity(UserBadge userBadge) {
+        return UserBadgeDto.builder()
+                .id(userBadge.getId())
+                .user(userBadge.getUser())
+                .badge(userBadge.getBadge())
+                .acquiredDate(userBadge.getAcquiredDate())
+                .build();
+    }
+}
