018e51e3afc11c6a0b47cf0f116b9240e9f633ef
Yang Sijun <yang@sijun.dev>
2025-04-22 19:48:35 +0900

    feat: User, Collection, Badge, UserBadge Entity 클래스 구현

---
+++ DiverBook_Server/src/main/java/ada/divercity/diverbook_server/entity/User.java
@@ -0,0 +1,48 @@
+package ada.divercity.diverbook_server.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "app_user")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    private UUID id;
+
+    @Column(nullable = false)
+    private String userName;
+
+    @Lob
+    private String userImage;
+
+    private String divisions;
+
+    private String phoneNumber;
+
+    private String interests;
+
+    private String places;
+
+    private String about;
+
+    @Column(nullable = false)
+    private String password;
+
+    private Float achievementRate;
+
+    @OneToMany(mappedBy = "owner", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Collection> collections = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<UserBadge> userBadges = new ArrayList<>();
+}
