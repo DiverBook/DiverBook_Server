250df6521b4a7488387d963475d1fa1ec219b109
Yang Sijun <yang@sijun.dev>
2025-04-25 20:48:14 +0900

    [feat] CollectionDto 수정

--- DiverBook_Server/src/main/java/ada/divercity/diverbook_server/dto/CollectionDto.java
+++ DiverBook_Server/src/main/java/ada/divercity/diverbook_server/dto/CollectionDto.java
@@ -1,10 +1,10 @@
 package ada.divercity.diverbook_server.dto;
 
 import ada.divercity.diverbook_server.entity.Collection;
-import ada.divercity.diverbook_server.entity.User;
 import lombok.*;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Getter
 @Setter
@@ -13,16 +13,16 @@ import java.time.LocalDateTime;
 @Builder
 public class CollectionDto {
     private Long id;
-    private User owner;
-    private User foundUser;
+    private UUID ownerId;
+    private UUID foundUserId;
     private LocalDateTime foundDate;
     private String memo;
 
     public static CollectionDto fromEntity(Collection collection) {
         return CollectionDto.builder()
                 .id(collection.getId())
-                .owner(collection.getOwner())
-                .foundUser(collection.getFoundUser())
+                .ownerId(collection.getOwner().getId())
+                .foundUserId(collection.getFoundUser().getId())
                 .foundDate(collection.getFoundDate())
                 .memo(collection.getMemo())
                 .build();
