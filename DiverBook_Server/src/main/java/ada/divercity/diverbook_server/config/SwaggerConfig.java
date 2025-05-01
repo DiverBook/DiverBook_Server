2b2abb6396c4acff764edbbaa8eba1da88a9ca50
Yang Sijun <yang@sijun.dev>
2025-05-02 01:17:01 +0900

    [fix] SwaggerConfig HTTPS 요청 설정

--- DiverBook_Server/src/main/java/ada/divercity/diverbook_server/config/SwaggerConfig.java
+++ DiverBook_Server/src/main/java/ada/divercity/diverbook_server/config/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package ada.divercity.diverbook_server.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
 @SecurityScheme(
@@ -9,6 +12,12 @@ import org.springframework.context.annotation.Configuration;
         type = SecuritySchemeType.HTTP,
         scheme = "bearer",
         bearerFormat = "JWT"
+)
+@OpenAPIDefinition(
+        info = @Info(title = "Diverbook API", version = "v1"),
+        servers = {
+                @Server(url = "https://diverbook.sijun.dev", description = "Production server")
+        }
 )
 @Configuration
 public class SwaggerConfig {
