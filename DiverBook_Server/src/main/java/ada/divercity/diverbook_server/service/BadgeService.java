8aa35feac5f94db00004e8ff2ccb541243a35801
Yang Sijun <yang@sijun.dev>
2025-05-01 15:46:17 +0900

    [feat] Badge 목록 및 정보 조회
    Badge 목록 및 정보 조회 API 구현

---
+++ DiverBook_Server/src/main/java/ada/divercity/diverbook_server/service/BadgeService.java
@@ -0,0 +1,9 @@
+package ada.divercity.diverbook_server.service;
+
+import ada.divercity.diverbook_server.dto.BadgeDto;
+
+import java.util.List;
+
+public interface BadgeService {
+    public List<BadgeDto> getAllBadges();
+}
---
+++ DiverBook_Server/src/main/java/ada/divercity/diverbook_server/service/BadgeServiceImpl.java
@@ -0,0 +1,23 @@
+package ada.divercity.diverbook_server.service;
+
+import ada.divercity.diverbook_server.dto.BadgeDto;
+import ada.divercity.diverbook_server.entity.Badge;
+import ada.divercity.diverbook_server.repository.BadgeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class BadgeServiceImpl implements BadgeService {
+    private final BadgeRepository badgeRepository;
+
+    public List<BadgeDto> getAllBadges() {
+        List<Badge> badges = badgeRepository.findAll();
+
+        return badges.stream()
+                .map(BadgeDto::fromEntity)
+                .toList();
+    }
+}
